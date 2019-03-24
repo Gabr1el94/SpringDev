d1b98b7f51b085d47a737e5097c47dccb943cb7d
Gabr1el94 <bielsystem64@gmail.com>
2019-03-24 16:12:05 -0300

    Execução spring por meio Main e Tests

---
+++ src/main/java/com/gabrieldev/springdev/SringDevApplication.java
@@ -0,0 +1,14 @@
+package com.gabrieldev.springdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class SringDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SringDevApplication.class, args);
+	}
+
+}
